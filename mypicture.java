59e222100f5fdfdc728ecb48f643ec8defddf05a
cartelkdot <47113944+cartelkdot@users.noreply.github.com>
2019-01-31 23:23:57 -0500

    Update mypicture.java

--- mypicture.java
+++ mypicture.java
@@ -12,8 +12,10 @@ public class mypicture
     private Square wall;
     private Square window;
     private Square window2;
+    private Square door;
     private Triangle roof;
     private Circle sun;
+    private Circle knob;
     private Person person;
     private boolean drawn;
     /**
@@ -27,6 +29,8 @@ public class mypicture
         sun = new Circle();
         person = new Person();
         window2 = new Square();
+        door= new Square();
+        knob = new Circle();
         drawn = false;
         
     }
@@ -44,32 +48,61 @@ public class mypicture
             wall.moveHorizontal(-140);
             wall.moveVertical(20);
             wall.changeSize(120);
-            wall.changeColor("blue");
+            wall.changeColor("red");
             wall.makeVisible();
             
+            
+            
+            
+            
+            door.moveHorizontal(-90);
+            door.moveVertical(120);
+            door.changeSize(20);
+            door.changeColor("black");
+            door.makeVisible();
+            
+            knob.moveHorizontal(1);
+            knob.moveVertical(159);
+            knob.changeSize(8);
+            knob.changeColor("yellow");
+            knob.makeVisible();
+            
+            
             window.changeColor("yellow");
-            window.moveHorizontal(-120);
+            window.moveHorizontal(-130);
             window.moveVertical(40);
             window.changeSize(40);
             window.makeVisible();
             
             
+            
+            
             window2.changeColor("yellow");
-            window2.moveHorizontal(-80);
-            window2.moveVertical(60);
+            window2.moveHorizontal(-70);
+            window2.moveVertical(40);
             window2.changeSize(40);
             window2.makeVisible();
             
             
+            person.changeColor("red");
+            person.moveHorizontal(-20);
+            person.moveVertical(-4);
+            person.changeSize(20, 10);
+            person.makeVisible();
+            
+            
+            roof.changeColor("black");
             roof.changeSize(60, 180);
             roof.moveHorizontal(20);
             roof.moveVertical(-60);
             roof.makeVisible();
             
-            person.moveHorizontal(0);
-            person.moveVertical(-20);
-            person.changeSize(20, 10);
-            person.makeVisible();
+             sun.changeColor("yellow");
+            sun.moveHorizontal(100);
+            sun.moveVertical(-40);
+            sun.changeSize(80);
+            sun.makeVisible();
+           
             
             
             
